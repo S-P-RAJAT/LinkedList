dc2102f4d7a107f359c78dc15a6d7c3f9d8948f8
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-01 00:23:27 +0530

    ADDED: Functionality to append elements to linked list

--- src/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -31,7 +31,26 @@ public class MyLinkedList<K> {
 		}
 		
 	}
-	
+	public void appendNode(INodeIF newNode)
+	{
+		if(this.head==null)
+		{
+			this.head=newNode;
+		}
+		if(this.tail==null)
+		{
+			this.tail=newNode;
+		}
+		else
+		{
+			
+			
+			this.tail.setNext(newNode);
+			this.tail=newNode;
+		}
+		
+		
+	}
 	public void  printNodes()
 	{
 		
